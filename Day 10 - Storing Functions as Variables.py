db297c62aaebf8280ccf5ca9be87e37a7eb1b4b5
Andilyn-N <146907015+Andilyn-N@users.noreply.github.com>
2024-12-04 23:47:22 -0600

    Create Day 10 - Storing Functions as Variables.py

---
+++ Day 10 - Storing Functions as Variables.py
@@ -0,0 +1,8 @@
+def add(n1, n2):
+    return n1 + n2
+
+# to tie a function to a variable, don't add "()"
+this_is_addition = add
+# to call the function, use the variable(input,input2)
+# will return 8 in the below example
+this_is_addition(3,5)
