575fa574df6bf27edca78fc655153858495a2467
Andilyn-N <146907015+Andilyn-N@users.noreply.github.com>
2024-12-05 21:02:44 -0600

    Create Day 11 - Blackjack To Dos and Rules.py

---
+++ Day 11 - Blackjack To Dos and Rules.py
@@ -0,0 +1,51 @@
+# TODO:
+
+# 2 players - computer and player
+# 2 cards for player
+# 1 card for computer
+
+# keep a running total of player cards
+# keep a running total of computer cards
+
+# if at any time, player == 21:
+# 	Is computer standing at 21?
+# 		If yes, standoff
+# 		If no, player wins
+
+# if player < 21 ask: Do you want to hit or stand?
+# 	if hit, draw another random card add to total, copmuter's turn
+# 	if stand, no more cards, computer's turn
+
+# If player cards > 21:
+# 	Check if ace in player hand
+# 		if yes, ace = 1 intead of 11, play continues
+# 		if no, player busts, computer wins
+
+# Computer "reveals" second card:
+# 	if computer == 21
+# 		is player standing?
+# 			yes: computer wins
+# 			no: player's turn
+# 	if 16 < computer < 21
+# 		computer stands
+# 	if computer > 21
+# 		check: does the computer have an ace?
+# 			if no, computer busts and player wins
+# 			if yes, ace == 1, play continues
+# 	if computer < 17
+# 		check: is player standing?
+# 			if no, player turn
+# 			if yes, computer draws
+
+# If player cards < 21, ask hit or stand:
+# 	if hit, draw another card
+# 	if stand, no more cards
+
+# If 21 > computer > player
+# 	Computer wins
+
+# If 21 > player > computer
+# 	Player wins
+
+# If 21 > player == computer > 16
+# 	Standoff
