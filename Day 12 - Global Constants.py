df83c4b765d8f6ea25057d869ca4962146d7c7cc
Andilyn-N <146907015+Andilyn-N@users.noreply.github.com>
2024-12-10 01:19:31 -0600

    Create Day 12 - Global Constants.py

---
+++ Day 12 - Global Constants.py
@@ -0,0 +1,6 @@
+# Global Constants:
+# You define them and never plan on changing them
+# These are put in all uppercase letters to indicate there are no changes
+
+PI = 3.14159
+GOOGLE_URL = "https://www.google.com"
