41baeac618467d503039bd175a535b92cffb3620
Andilyn-N <146907015+Andilyn-N@users.noreply.github.com>
2024-12-13 13:45:34 -0600

    Name updates

--- Day 14 - Higher or Lower Project - Instagram Followers Comparison.py
+++ Day 14 - Higher or Lower Project - Instagram Followers Comparison.py
@@ -1,54 +1,86 @@
+logo = r"""
+    __  ___       __             
+   / / / (_)___ _/ /_  ___  _____
+  / /_/ / / __ `/ __ \/ _ \/ ___/
+ / __  / / /_/ / / / /  __/ /    
+/_/ ///_/\__, /_/ /_/\___/_/     
+   / /  /____/_      _____  _____
+  / /   / __ \ | /| / / _ \/ ___/
+ / /___/ /_/ / |/ |/ /  __/ /    
+/_____/\____/|__/|__/\___/_/     
+"""
+
+vs = r"""
+ _    __    
+| |  / /____
+| | / / ___/
+| |/ (__  ) 
+|___/____(_)
+"""
+
 from game_data import data
-from art import vs
-from art import logo
 import random
 
-# print(logo)
+print(logo)
 
-def data_values(account):
-    name = account["name"]
-    description = account["description"]
-    country = account["country"]
-    return f"{name}, a {description} from {country}"
+def chooser(account_1, account_2):
+    name_1 = account_1["name"]
+    description_1 = account_1["description"]
+    followers_1 = account_1["follower_count"]
+    country_1 = account_1["country"]
+    name_2 = account_2["name"]
+    description_2 = account_2["description"]
+    followers_2 = account_2["follower_count"]
+    country_2 = account_2["country"]
 
-keys = list(data)
-choice_A = random.choice(keys)
+    print(f"Compare A: {name_1}, a {description_1} from {country_1}.")
+    print(vs)
+    print(f"Against B: {name_2}, a {description_2} from {country_2}.\n")
+    return followers_1, followers_2
 
-print(f"Compare A: {data_values(choice_A)}")
 
-# print(vs)
+def assign(choice_1, choice_2):
+    og_choice_2 = choice_2
+    while choice_1 == choice_2 or og_choice_2 == choice_2:
+        choice_2 = random.choice(data)
+    return [choice_1, choice_2]
 
-choice_B = random.choice(keys)
 
-print(f"Against B: {data_values(choice_B)}")
 
 def game():
     score = 0
     game_continue = True
+    random_choice_1 = random.choice(data)
+    random_choice_2 = None
     while game_continue is True:
+        random_choice_1, random_choice_2 = assign(random_choice_1, random_choice_2)
+        followers_1, followers_2 = chooser(random_choice_1, random_choice_2)
+        print(f"Your score is {score}.\n")
         player_guess = input("Who has more Instagram followers? Type \"A\" or \"B\":  ").upper()
-        if choice_A > choice_B and player_guess == "A":
+
+        if followers_1 > followers_2 and player_guess == "A":
             score += 1
-            print(f"That's right. Your score is {score}.")
-            switch()
-        elif choice_A < choice_B and player_guess == "B":
+            print(f"\n\n\nThat's right.")
+            random_choice_1 = random_choice_2
+
+        elif followers_1 < followers_2  and player_guess == "B":
             score += 1
-            print(f"That's right. Your score is {score}.")
-            switch()
-        elif choice_A > choice_B and player_guess == "B":
-            print(f"Game over. Final score: {score}")
+            print(f"\n\n\nThat's right.")
+            random_choice_1 = random_choice_2
+
+        elif followers_1 > followers_2  and player_guess == "B":
+            print(f"Wrong. Game over. Score: {score}")
             game_continue = False
-        elif choice_A < choice_B and player_guess == "A":
-            print(f"Game over. Final score: {score}")
+
+        elif followers_1 < followers_2 and player_guess == "A":
+            print(f"Wrong. Game over. Score = {score}")
             game_continue = False
+
         else:
-            print("Error 1.")
+            print(f"Invalid entry. Game over. Score = {score}")
             game_continue = False
 game()
 
-# if right, move B person to A position
 
-
-# if right, loop back to move B to A
 
 
